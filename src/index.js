bdf2b911c075c224bfaf88d923c880e40bc369fe
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-10-21 11:44:31 +0530

    Added redux-persist so that user account  data still there when page refresh

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import {store} from  "./redux/store";
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+import "./index.css";
+import App from "./App";
+
+let persistor = persistStore(store);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>    
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./slices/accountSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+}
+
+const reducer = combineReducers({
+    account: accountReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
 
 export const store = configureStore({
-    reducer: {
-        account: accountReducer,
-    },
+    reducer: persistedReducer,
     devTools: true,
 });
